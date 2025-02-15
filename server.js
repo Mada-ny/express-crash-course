5befcf88cb1f14eaee862b868cc34f8ae5e478ed
Madany Doumbia <madany.doumbia@epitech.eu>
2025-02-15 17:59:15 +0000

    Express routing

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     console.log("Here");
-    res.render("index", { text23423: "World" });
+    res.render("index", { text: "World" });
 });
+
+const userRouter = require('./routes/users');
+
+app.use('/users', userRouter);
 
 app.listen(3000);
